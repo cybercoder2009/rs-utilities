8220c97d55dcc5aa639f8002f775f124e0fe5c38
cybercoder2009 <cybercoder2009@gmail.com>
2024-02-29 22:29:25 -0700

    add rand_f32

--- src/rand.rs
+++ src/rand.rs
@@ -1,6 +1,11 @@
-use rand::{Rng, thread_rng, rngs::ThreadRng};
+use rand::{Rng, thread_rng, rngs::ThreadRng, distributions::Uniform};
 
 pub fn rand_usize(min: usize, max: usize) -> usize {
+    let mut rng: ThreadRng = thread_rng();
+    rng.gen_range(min..=max)
+}
+
+pub fn rand_u32(min: u32, max: u32) -> u32 {
     let mut rng: ThreadRng = thread_rng();
     rng.gen_range(min..=max)
 }
@@ -25,4 +30,12 @@ pub fn rand_alphabets(min: usize, max: usize) -> String {
         characters.chars().nth(idx).unwrap()
     })
     .collect()
+}
+
+pub fn rand_f32(min: f32, max: f32, precision: usize) -> f32 {
+    let range = Uniform::new_inclusive(min, max);
+    let mut rng = rand::thread_rng();
+    let rnd: f32 = rng.sample(range);
+    let rnd_str = format!("{:.*}", precision, rnd);
+    rnd_str.parse::<f32>().unwrap()
 }
