c9ec694cd0a7655d03297410fb2264e1c82dc633
cybercoder2009 <cybercoder2009@gmail.com>
2024-02-29 22:29:34 -0700

    update

--- src/convert.rs
+++ src/convert.rs
@@ -15,3 +15,29 @@ pub fn str_to_u8_64(s: &str) -> [u8; 64] {
     }
     array
 }
+
+pub fn hex_str_to_u8_32(hex_str: &str) -> Result<[u8; 32], std::num::ParseIntError> {
+    let trucated: &str = if hex_str.len() > 64 { &hex_str[..64] } else { hex_str };
+    let mut array: [u8; 32] = [0u8; 32];
+    for (i, byte) in trucated.as_bytes().chunks(2).enumerate() {
+        array[i] = u8::from_str_radix(std::str::from_utf8(byte).unwrap(), 16)?;
+    }
+    Ok(array)
+}
+
+pub fn u8_32_to_hex_string(bytes: &[u8; 32]) -> String {
+    bytes.iter().map(|byte| format!("{:02x}", byte)).collect()
+}
+
+pub fn hex_str_to_u8_64(hex_str: &str) -> Result<[u8; 64], std::num::ParseIntError> {
+    let trucated: &str = if hex_str.len() > 128 { &hex_str[..128] } else { hex_str };
+    let mut array: [u8; 64] = [0u8; 64];
+    for (i, byte) in trucated.as_bytes().chunks(2).enumerate() {
+        array[i] = u8::from_str_radix(std::str::from_utf8(byte).unwrap(), 16)?;
+    }
+    Ok(array)
+}
+
+pub fn u8_64_to_hex_string(bytes: &[u8; 64]) -> String {
+    bytes.iter().map(|byte| format!("{:02x}", byte)).collect()
+}
